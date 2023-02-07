4e555e49c8d2d846e7cc7cc9dc4d11f4d0c98162
jonghyun ha <hy75212@naver.com>
2023-02-07 16:41:51 +0900

    -

--- Going.Boards/GoingPLC.cs
+++ Going.Boards/GoingPLC.cs
@@ -1,6 +1,7 @@
-﻿using Devinno.PLC.Ladder;
+﻿using Devinno.Communications.TextComm.TCP;
+using Devinno.Data;
+using Devinno.PLC.Ladder;
 using Going.Boards.Interfaces;
-using System;
 using System.Collections.Generic;
 using Unosquare.RaspberryIO;
 using Unosquare.WiringPi;
@@ -13,8 +14,31 @@ namespace Going.Boards
         public List<IGoingBoard> Boards { get; } = new List<IGoingBoard>();
         #endregion
 
+        TextCommTCPSlave comm;
+
         public GoingPLC()
-        {            
+        {
+            #region Communication
+            comm = new TextCommTCPSlave { DisconnectCheckTime = 3000 };
+            comm.Start();
+            comm.MessageRequest += (o, s) => {
+
+                if (s.Command == 10)
+                {
+                    try
+                    {
+                        var v = Serialize.JsonDeserialize<List<Shield>>(s.RequestMessage);
+                        if (v != null && v.Count > 0)
+                        {
+
+                        }
+                    }
+                    catch { }
+                }
+
+            };
+            #endregion
+            #region PLC
             DeviceLoad += (o, s) =>
             {
                 foreach (var v in Boards)
@@ -32,17 +56,28 @@ namespace Going.Boards
                     v.Out();
                 }
             };
+            #endregion
         }
 
         #region Method
         #region Initialize
         public void Initialize()
         {
-            Pi.Init<BootstrapWiringPi>();
+            try
+            {
+                Pi.Init<BootstrapWiringPi>();
+            }
+            catch { }
 
             foreach (var v in Boards) v.Begin();
         }
         #endregion
         #endregion
+    }
+
+    class Shield
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
