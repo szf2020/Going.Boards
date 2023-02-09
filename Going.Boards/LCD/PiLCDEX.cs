226d3ae59c901d086d3fa14c8184952365105aea
GoingkR <goinggithub@gmail.com>
2023-02-09 19:04:07 +0900

    .

---
+++ Going.Boards/LCD/PiLCDEX.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unosquare.RaspberryIO.Abstractions;
+using Unosquare.RaspberryIO;
+using Going.Boards.Interfaces;
+
+namespace Going.Boards.LCD
+{
+    public class PiLCDEX : GoingBoard
+    {
+        #region Properties
+        public override bool[] Input { get; } = new bool[4];
+        public override bool[] Output { get; } = new bool[5];
+        #endregion
+
+        #region Member Variable
+        IGpioPin[] Outs = new IGpioPin[5];
+        IGpioPin[] Ins = new IGpioPin[4];
+        #endregion
+
+        #region Constructor
+        public PiLCDEX()
+        {
+            Ins[0] = Pi.Gpio[P1.Pin38]; Ins[0].PinMode = GpioPinDriveMode.Input;
+            Ins[1] = Pi.Gpio[P1.Pin40]; Ins[1].PinMode = GpioPinDriveMode.Input;
+            Ins[2] = Pi.Gpio[P1.Pin15]; Ins[2].PinMode = GpioPinDriveMode.Input;
+            Ins[3] = Pi.Gpio[P1.Pin16]; Ins[3].PinMode = GpioPinDriveMode.Input;
+
+            Outs[0] = Pi.Gpio[P1.Pin29]; Outs[0].PinMode = GpioPinDriveMode.Output;
+            Outs[1] = Pi.Gpio[P1.Pin31]; Outs[1].PinMode = GpioPinDriveMode.Output;
+            Outs[2] = Pi.Gpio[P1.Pin32]; Outs[2].PinMode = GpioPinDriveMode.Output;
+            Outs[3] = Pi.Gpio[P1.Pin33]; Outs[3].PinMode = GpioPinDriveMode.Output;
+
+            Outs[4] = Pi.Gpio[P1.Pin22]; Outs[4].PinMode = GpioPinDriveMode.Output;
+        }
+        #endregion
+
+        #region Method
+        #region Begin
+        public override void Begin()
+        {
+            Load();
+            Out();
+        }
+
+        public void Begin(byte in_byte, byte out_byte)
+        {
+            Begin();
+        }
+        #endregion
+
+        #region Load
+        public override void Load()
+        {
+            for (int i = 0; i < 4; i++)
+                Input[i] = Ins[i].Read();
+        }
+        #endregion
+
+        #region Out
+        public override void Out()
+        {
+            for (int i = 0; i < 5; i++)
+                Outs[i].Write(Output[i]);
+        }
+        #endregion
+
+        #endregion
+    }
+}
--- RpiPLC/Program.cs
+++ RpiPLC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Going.Boards;
+using Going.Boards.LCD;
 using System;
 
 namespace RpiPLC
@@ -12,15 +13,19 @@ namespace RpiPLC
             engine.Initialize();
 
             #region External Board
-            var EXSD8I = new SD8I();
-            var EXSD8R = new SD8R();
+            //var EXSD8I = new SD8I();
+            //var EXSD8R = new SD8R();
+            var LCDEX = new PiLCDEX();
             for (int i = 0; i < 8; i++)
             {
-                EXSD8I.InputMap.Add(i, "P" + (i + 1));
-                EXSD8R.OutputMap.Add(i, "P" + (i + 11));
+                //EXSD8I.InputMap.Add(i, "P" + (i + 1));
+                //EXSD8R.OutputMap.Add(i, "P" + (i + 11));
+                LCDEX.InputMap.Add(i, "P" + (i + 1));
+                LCDEX.OutputMap.Add(i, "P" + (i + 11));
             }
-            engine.Boards.Add(EXSD8I);
-            engine.Boards.Add(EXSD8R);
+            //engine.Boards.Add(EXSD8I);
+            //engine.Boards.Add(EXSD8R);
+            engine.Boards.Add(LCDEX);
             #endregion
             engine.Start();
             #endregion
